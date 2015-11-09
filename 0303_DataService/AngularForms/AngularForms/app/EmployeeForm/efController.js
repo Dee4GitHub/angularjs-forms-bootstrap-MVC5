207d81a7259130f90ca49d4fcc76f67835751bf6
deepak4u <deepak.papanna@gmail.com>
2015-11-09 00:26:39 +0000

    Finished lesson 03 Adding a Data Service of the Chapter 03 Using Forms in AngularJS Apps.

---
+++ 0303_DataService/AngularForms/AngularForms/app/EmployeeForm/efController.js
@@ -0,0 +1,8 @@
+﻿angularFormsApp.controller('efController', function efController($scope, DataService) {
+    $scope.employee = DataService.employee;
+    $scope.departments = ["Administration","Engineering", "Finance", "Marketing"];
+
+    $scope.SubmitClick = function() {
+        console.log($scope.employee);
+    }
+});
