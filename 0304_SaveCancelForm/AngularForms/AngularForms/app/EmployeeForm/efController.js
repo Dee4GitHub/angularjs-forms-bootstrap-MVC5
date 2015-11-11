d1340ffb11dcc4ec764af5a47578faba74ccd114
deepak4u <deepak.papanna@gmail.com>
2015-11-11 02:27:31 +0000

    Finished lesson 04  Saving or Cancelling a Form of the Chapter 03 Using Forms in AngularJS Apps.

---
+++ 0304_SaveCancelForm/AngularForms/AngularForms/app/EmployeeForm/efController.js
@@ -0,0 +1,15 @@
+﻿angularFormsApp.controller('efController', function efController($scope,$window, DataService) {
+    $scope.employee = DataService.employee;
+    $scope.departments = ["Administration","Engineering", "Finance", "Marketing"];
+    $scope.editableEmployee = angular.copy($scope.employee);
+    $scope.SubmitForm= function () {
+        $scope.employee = angular.copy($scope.editableEmployee);
+        $window.history.back();
+        //console.log("submit button clicked");
+    }
+
+    $scope.CancelForm = function () {
+        $window.history.back();
+        //console.log("cancel button clicked");
+    }
+});
---
+++ 0304_SaveCancelForm/Readme.md
@@ -0,0 +1,12 @@
+Welcome to AngularJS Forms using Bootstrap and MVC5 Demo
+
+This folder contains the latest code (as of 18th October 2015) for the Mark Zamoyta's' Course - AngularJS Forms using Bootstrap and MVC5.
+
+Further this folder contains code for the 3rd Chapter Using Forms in AngularJS Apps - Lesson 04 Saving or Cancelling a Form
+
+Please feel free to download and use the code for your learning or other responsible use.
+
+
+Regards, 
+
+Deepak Papanna
