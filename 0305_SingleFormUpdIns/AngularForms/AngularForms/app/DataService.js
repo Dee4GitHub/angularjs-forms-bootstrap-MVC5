e01883aeedc70f6ac733420e5e328e85d6eb1562
deepak4u <deepak.papanna@gmail.com>
2015-11-23 22:17:52 +0000

    Finished lesson 05  Single form update insert  of the Chapter 03 Using Forms in AngularJS Apps.

---
+++ 0305_SingleFormUpdIns/AngularForms/AngularForms/app/DataService.js
@@ -0,0 +1,32 @@
+﻿angularFormsApp.factory('DataService',
+    function () {
+        var getEmployee = function(id) {
+            if (id == 123) {
+                return {
+                    fullName: "Milton Waddams",
+                    notes: "The ideal employee.  Just don't touch his red stapler.",
+                    department: "Administration",
+                    perkCar: true,
+                    perkStock: false,
+                    perkSixWeeks: true,
+                    payrollType: "none"
+                };
+            }
+            return undefined;
+        };
+
+        var insertEmployee = function(newEmployee) {
+            return true;
+        };
+
+        var updateEmployee = function(employee) {
+            return true;
+        };
+
+        return {
+            insertEmployee: insertEmployee,
+            getEmployee: getEmployee,
+            updateEmployee: updateEmployee
+        };
+    });
+
