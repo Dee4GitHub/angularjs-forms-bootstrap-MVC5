821013eb80086328542b566ae8c5132838093691
deepak4u <deepak.papanna@gmail.com>
2015-11-25 11:45:35 +0000

    Finished lesson 06 Modal Forms in AngularJS  of the Chapter 03 Using Forms in AngularJS Apps.

---
+++ 0306_ModalForms/AngularForms/AngularForms/app/EmployeeForm/efController.js
@@ -0,0 +1,29 @@
+﻿angularFormsApp.controller('efController', function efController($scope, $window, $routeParams, $uibModalInstance, DataService) {
+    if ($routeParams.id) {
+        $scope.employee = DataService.getEmployee($routeParams.id);
+    } else {
+        $scope.employee = { id: 0 };
+    }
+    
+    $scope.departments = ["Administration","Engineering", "Finance", "Marketing"];
+    $scope.editableEmployee = angular.copy($scope.employee);
+    $scope.SubmitForm = function () {
+        if ($scope.editableEmployee.id == 0) {
+            //Insert new employee
+            DataService.insertEmployee($scope.editableEmployee);
+        } else {
+            //Update the employee
+            DataService.updateEmployee($scope.editableEmployee);
+        }
+        $scope.employee = angular.copy($scope.editableEmployee);
+        $uibModalInstance.close();
+        //$window.history.back();
+        //console.log("submit button clicked");
+    }
+
+    $scope.CancelForm = function () {
+        $uibModalInstance.dismiss();
+        //$window.history.back();
+        //console.log("cancel button clicked");
+    }
+});
